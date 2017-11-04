5a37b15d3786d12b6ca8b9684c10bbb42f9e3599
airvip <sdqhwzb@163.com>
2017-11-05 01:53:48 +0800

    my first vue2.0-demo

---
+++ 20171102/vue-demo/components/News.vue
@@ -0,0 +1,31 @@
+<template>
+	<div id="news">
+		<h3>i am news page</h3>
+		<ol>
+			<li v-for="(val,index) in news" v-text="val"></li>
+		</ol>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'news',
+		data(){
+			return {
+				news:[
+					'1111111111',
+					'2222222222',
+					'3333333333',
+					'4444444444',
+					'5555555555'
+				]
+			}
+		}
+	}
+</script>
+
+<style scoped>
+    li{
+    	border-bottom: 1px dashed #f60;
+    }	
+</style>
