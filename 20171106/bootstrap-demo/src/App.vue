626897622255cd5cad047dab366b0957ddcafa32
airvip <sdqhwzb@163.com>
2017-11-06 23:56:19 +0800

    add element-demo

---
+++ 20171106/bootstrap-demo/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <button class="btn btn-success">成功按钮</button>
+    <button class="btn btn-danger">危险按钮</button>
+
+    <div class="row">
+    	<div class="col-sm-8 innercolor1">8</div>
+    	<div class="col-sm-4 innercolor2">4</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to My App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.innercolor1{
+    background: #f60;
+}
+.innercolor2{
+    background: #360;
+}
+</style>
