7c0828d46bf156f001b09aa7387ca8b0b85b0ecc
airvip <sdqhwzb@163.com>
2017-11-08 00:21:13 +0800

    reflash npm run dev

--- 20171106/element-demo/src/App.vue
+++ 20171106/element-demo/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     {{msg}}
+    <el-button type="primary">主要按钮</el-button>
   </div>
 </template>
 
--- 20171106/element-demo/webpack.config.js
+++ 20171106/element-demo/webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
       },*/
       {
           test: /\.css$/,
-          loader: 'css-loader'
+          loader: 'style-loader!css-loader'
       }, 
       {
           test: /\.(eot|woff|woff2|ttf)([\?]?.*)$/,
