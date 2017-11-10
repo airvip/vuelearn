37aaf88835079c434f69773b2caf6ebfe0f16abb
airvip <sdqhwzb@163.com>
2017-11-10 23:19:43 +0800

    customcomponent

---
+++ 20171110/custom/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    {{msg}}
+    <Loading></Loading>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ 20171110/custom/src/components/loading/Loading.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="myLoading">
+		{{msg}}
+	</div>
+</template>
+
+<style scoped>
+	.myLoading{
+		color: #F60;
+		font-size: 40px;
+		text-shadow: 2px 2px 5px #000;
+	}
+</style>
+
+<script>
+	export default{
+		data(){
+			return {
+				msg:'Loading...'
+			}
+		}
+	}
+</script>
