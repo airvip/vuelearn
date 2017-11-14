8de254701a32cef310b22730b36ca745cbcfbb36
airvip <sdqhwzb@163.com>
2017-11-14 23:53:36 +0800

    add all

--- 20171112/vue-news/src/App.vue
+++ 20171112/vue-news/src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <div id="app">
-
     <div id="header">
-      <mt-navbar class="page-part" v-model="selected" :active.sync="selected">
+      <mt-navbar class="page-part" v-model="selected">
         <mt-tab-item id="1">选项一</mt-tab-item>
         <mt-tab-item id="2">选项二</mt-tab-item>
         <mt-tab-item id="3">选项三</mt-tab-item>
       </mt-navbar>
     </div>
-    {{selected}}
-    <div id="swipe" class="page-swipe">
+
+    <div id="swipe" >
       <mt-swipe :auto="4000">
         <mt-swipe-item :class="{slide1:true}">1</mt-swipe-item>
         <mt-swipe-item :class="{slide1:true}">2</mt-swipe-item>
@@ -19,20 +18,18 @@
 
     <div id="content">
       <!-- tab-container -->
-      <mt-tab-container v-model="selected"  :active.sync="selected">
+      <mt-tab-container v-model="selected">
         <mt-tab-container-item id="1">
-          <mt-cell  :title="'内容 ' + 1"></mt-cell>
+          <mt-cell v-for="n in 3" >{{n}}</mt-cell>
         </mt-tab-container-item>
         <mt-tab-container-item id="2">
-          <mt-cell  :title="'测试 ' + 2"></mt-cell>
+          <mt-cell v-for="n in 2" >{{n}}</mt-cell>
         </mt-tab-container-item>
         <mt-tab-container-item id="3">
-          <mt-cell  :title="'选项 ' + 3"></mt-cell>
+          <mt-cell v-for="n in 4" >{{n}}</mt-cell>
         </mt-tab-container-item>
       </mt-tab-container>
     </div>
-      
-    
 
     <!-- footer -->
     <mt-tabbar>
@@ -59,8 +56,7 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome',
-      selected:1
+      selected:'1'
     }
   }
 }
@@ -69,66 +65,25 @@ export default {
 <style lang="scss">
 body{margin: 0;padding: 0;}
 
-.page-swipe .mint-swipe{
+.mint-swipe{
   height:200px;
-  color:#fff;
   font-size:30px;
   text-align:center;
 }
-.page-swipe .mint-swipe{
-  height:200px;
-  color:#fff;
-  font-size:30px;
-  text-align:center;
-  margin-bottom: 20px;
-}
-.page-swipe .mint-swipe-item{
+.mint-swipe-item{
   line-height: 200px;
+  color:#FFF;
 }
-.page-swipe .slide1{
+.slide1{
   background-color: #f60;
-  color: #fff; 
 }
-.page-swipe .slide2{
+.slide2{
   background-color: #960;
-  color: #fff; 
 }
-.page-swipe .slide3{
+.slide3{
   background-color: #360;
-  color: #fff; 
-}
-.page-swipe-desc{
-  text-align: center;
-  color:#666;
-  margin-bottom: 5px;
 }
 
 
-.page-part {
-    margin-bottom: 3px;
-}
-.mint-navbar {
-    background-color: #fff;
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    text-align: center;
-}
-.mint-navbar .mint-tab-item.is-selected {
-    border-bottom: 3px solid #26a2ff;
-    color: #26a2ff;
-    margin-bottom: -3px;
-}
-.mint-navbar .mint-tab-item {
-    padding: 17px 0;
-    font-size: 15px;
-}
-.mint-tab-item {
-    display: block;
-    padding: 7px 0;
-    -webkit-box-flex: 1;
-    -ms-flex: 1;
-    flex: 1;
-    text-decoration: none;
-}
+
 </style>
