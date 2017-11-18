7f7c175e137a1a19f01084badd7e0361eab60794
airvip <sdqhwzb@163.com>
2017-11-18 23:19:58 +0800

    add router.config.js

--- 20171112/vue-news/src/router.config.js
+++ 20171112/vue-news/src/router.config.js
@@ -3,16 +3,9 @@ import Follow from './components/Follow.vue'
 import Column from './components/Column.vue'
 import Userinfo from './components/Userinfo.vue'
 
-export default [{
-	path:'/home',
-	component:Home
-},{
-	path:'/follow',
-	component:Follow
-},{
-	path:'/column',
-	component:Column
-},{
-	path:'/user-info',
-	component:Userinfo
-}];
+export default [
+{path:'*',redirect:'/home'},
+{path:'/home',component:Home},
+{path:'/follow',component:Follow},
+{path:'/column',component:Column},
+{path:'/user-info',component:Userinfo}];
