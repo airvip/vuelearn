13810eca422af3a5d9dfe0e8283825a3f154843f
airvip <sdqhwzb@163.com>
2017-12-12 00:18:39 +0800

    ejs test

--- 20171210/reader/app.js
+++ 20171210/reader/app.js
@@ -2,12 +2,23 @@ var koa = require('koa');
 var route = require('koa-route');
 var app = koa(); 
 
+
+var views = require('co-views');
+var render = views('./view',{
+	map:{html:'ejs'}
+});
+
+
 // console.log(controller);
 // return;
 
 app.use(route.get('/route_test',function*(){
 	this.set('Cache-Control','no-cache');
 	this.body = 'hello koa';
+}));
+app.use(route.get('/ejs_test',function*(){
+	this.set('Cache-Control','no-cache');
+	this.body = yield render('test',{title:'title_test'});
 }));
 
 app.listen(3001);
